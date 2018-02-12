5d6dd08821d69a38c2e09b10acdabe70c9ec822c
Artas47 <artasss47@gmail.com>
2018-02-12 18:43:53 +0100

    made chart a component, added other charts with units

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
 
+
 class SearchBar extends Component{
   constructor(props){
     super(props);
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,35 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Sparklines, SparklinesLine} from 'react-sparklines';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component{
 	renderWeather(cityData){
 
 		const name = cityData.city.name;
 		const temps = cityData.list.map((weather) => weather.main.temp);
+		const pressures = cityData.list.map((weather) => weather.main.pressure);
+		const humidities= cityData.list.map((weather) => weather.main.humidity);
+		const {lon, lat} = cityData.city.coord;
+		//SAME AS const lon = cityData.city.coord.lon; 
+		//const lat = cityData.city.coord.lat;
+
 
 		return (
 			<tr key={name}>
-				<td>{name}</td>
-				<td>
-					<Sparklines height={120} width={180} data={temps}>
-						<SparklinesLine color='blue' />
-					</Sparklines>
-				</td>
-			</tr>
+				<td> <GoogleMap lon={lon} lat={lat}/></td>
+				<td> <Chart color='red' data={temps} units='K'/> </td>
+				<td> <Chart color='orange' data={pressures} units='hPa'/> </td>
+				<td> <Chart color='green' data={humidities} units='%'/> </td>
+			</tr> 
 		)
 	}
+
     
   render(){
 
     return(
       <table className='table table-hover'>
         <thead>
-          <tr>
+          <tr> 
           	<th>City</th>
-            <th>Temperatur</th>
-            <th>Preasure</th>
-            <th>Humidity</th>
+            <th>Temperatur (K)</th>
+            <th>Preasure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
           </thead>
           <tbody>
@@ -37,7 +43,7 @@ class WeatherList extends Component{
           </tbody>  
       </table>
 			)
-		}
+	}
 }
 
 function mapStateToProps({weather}){  // SAME AS state (not rly)
